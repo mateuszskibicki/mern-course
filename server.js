0350f9c821d2efacbc10fe2a87d986368c44ff3d
Mateusz Skibicki <skibickiengland@gmail.com>
2018-05-28 11:16:09 +0100

    jwt

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 
-//routes
+//passport
+const passport = require('passport');
+
+// bodyParser
+const bodyParser = require('body-parser');
+
+// routes
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
@@ -11,19 +17,25 @@ const posts = require('./routes/api/posts');
 // Initialize app
 const app = express();
 
-//DB Config
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// DB Config
 const db = require('./config/keys').mongoURI;
 
-//Connect to MongoDB
+// Connect to MongoDB
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 
-app.get('/', (req, res) => {
-  res.send('Hellsso xD222222');
-});
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
 
 app.use('/api/users', users);
 app.use('/api/profile', profile);
