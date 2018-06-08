8c4329f3f1c7d903d263f4024c0abdbaf432a8e9
Mateusz Skibicki <skibickiengland@gmail.com>
2018-06-08 18:58:39 +0100

    Store.js changed

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,6 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 const initialState = {};
 
@@ -10,7 +9,10 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  compose(
+    applyMiddleware(...middleware)
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
